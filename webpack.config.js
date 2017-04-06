83c7c9567dd9c66bfb85c95d7523c078bfb41213
Paul Pineda <paul@pineda.codes>
2017-04-06 20:03:55 +0100

    added ignore, webpack.config and yarn.lock

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: path.resolve(__dirname, './'),
+  entry: {
+    app: './index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+    publicPath: path.resolve(__dirname, './')
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './')
+  }
+}
